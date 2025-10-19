c691b43fa5b781360f704b0ee5a0ac58a4422e97
RAGHAVVRAM JOHNSON <raghavvram.johnson@gmail.com>
2025-10-19 21:54:00 +0530

    improved output.py

--- lib/output.py
+++ lib/output.py
@@ -1,41 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class color:
+    GREEN = '\033[32m'
+    CYAN = '\033[36m'
+    RED = '\033[31m'
+    YELLOW = '\033[33m'
+    GRAY = '\033[90m'
+    BOLD = '\033[1m'
+    RESET = '\033[0m'
+
+
 def info(text):
-    print(
-        "[\033[1m\033[32m+\033[0m] {}".format(
-            text
-        )
-    )
+    print(f"{color.BOLD}{color.GREEN}[+]{color.RESET} {text}")
 
 
 def prompt(text, lowercase=True):
-    question = input(
-        "[\033[1m\033[36m?\033[0m] {}: ".format(
-            text
-        )
-    )
+    question = input(f"{color.BOLD}{color.CYAN}[?]{color.RESET} {text}: ")
     if lowercase:
         return question.lower()
     return question
 
 
 def error(text):
-    print(
-        "[\033[1m\033[31m!\033[0m] {}".format(
-            text
-        )
-    )
+    print(f"{color.BOLD}{color.RED}[!]{color.RESET} {text}")
 
 
 def warning(text):
-    print(
-        "[\033[1m\033[33m-\033[0m] {}".format(
-            text
-        )
-    )
+    print(f"{color.BOLD}{color.YELLOW}[-]{color.RESET} {text}")
 
 
 def misc_info(text):
-    print(
-        "[\033[90mi\033[0m] {}".format(
-            text
-        )
-    )
+    print(f"{color.GRAY}[i]{color.RESET} {text}")
